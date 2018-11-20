b57771d273725b74128092ce4d92021fe62d017c
faturita <rramele@gmail.com>
2018-11-20 17:27:40 -0300

    Small changes

--- plotprocessing.h
+++ plotprocessing.h
@@ -8,3 +8,4 @@ void process();
 void comparesignals(int subject1, int epoch1, int label1, int channel1, int subject2, int epoch2, int label2, int channel2);
 void comparehits();
 void classify(float *descr);
+void memorize(float *descr, int length);
